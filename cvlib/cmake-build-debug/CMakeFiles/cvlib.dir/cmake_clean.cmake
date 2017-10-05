7445393a09ab58d1747ce3c62b528a71bf1f9392
DavidH <HeyItsDavid@users.noreply.github.com>
2017-10-05 13:57:44 +0100

    Initial Commit - fixed
    past commit was shite

---
+++ cvlib/cmake-build-debug/CMakeFiles/cvlib.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cvlib.dir/library.cpp.o"
+  "libcvlib.pdb"
+  "libcvlib.dylib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cvlib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ drone/README.md
@@ -0,0 +1,1 @@
+Drone control stuff goes here
