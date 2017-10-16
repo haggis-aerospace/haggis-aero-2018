a3dfb194d1725134e0cbe8b9060ba102ea86741c
Michael Reid <mreid@dundee.ac.uk>
2017-10-16 16:22:36 +0100

    Python structure update

--- drone/droneCode.py
+++ drone/droneCode.py
@@ -3,12 +3,18 @@ import dronekit
 import exceptions
 import socket
 import time
-import navigation as nav
+from navigation import NavClass
 from threading import Thread
+import binding
 
 
+nav = None
+vehicle = None
 
-def main(vehicle):
+def main():
+    global nav
+    global vehicle
+
     print "Starting simulator (SITL)"
 
     # Get some vehicle attributes (state)
@@ -20,12 +26,10 @@ def main(vehicle):
     print " System status: %s" % vehicle.system_status.state
     print " Mode: %s" % vehicle.mode.name    # settable
 
-    #Enable communication through another Ground station
-
-
     #Limit vehicles speed (m/s)
     vehicle.airspeed = 2
     vehicle.groundspeed = 2
+    print "Max Speed Set"
 
     #Loop contains all instructions drone will execute
     while True:
@@ -34,8 +38,9 @@ def main(vehicle):
         while vehicle.mode.name != "GUIDED":
             time.sleep(2)
         if vehicle.armed == False: # Arm drone and climb to 10m once in guided mode
-            nav.arm_and_takeoff(vehicle, 10)
-            nav.goto(vehicle, 1, 1)
+            print "Arming..."
+            nav.arm_and_takeoff(10)
+            nav.goto(1, 1)
 
         #Main Program execution code
         if vehicle.mode.name == "GUIDED":
@@ -44,10 +49,10 @@ def main(vehicle):
             for i in range(1,3):
                 flySquare(5)
 
-            nav.goto(vehicle, 20,30)
-            nav.goto(vehicle, -40,0)
+            nav.goto(20,30)
+            nav.goto(-40,0)
 
-            nav.returnToLaunch(vehicle)
+            nav.returnToLaunch()
 
             time.sleep(0.2)
         else:
@@ -60,21 +65,29 @@ def main(vehicle):
 
 #Spin 180 degrees
 def spinRound():
-    nav.condition_yaw(vehicle, 180, relative=True)
+    nav.condition_yaw(180, relative=True)
 
 #Fly in a square
 def flySquare(dist=5):
-    nav.goto(vehicle, dist, 0, 0.5, 1)
-    nav.goto(vehicle, 0, dist, 0.5, 1)
-    nav.goto(vehicle, dist*-1, 0, 0.5, 1)
-    nav.goto(vehicle, 0, dist*-1, 0.5, 1)
+    nav.goto(dist, 0, 0.5, 1)
+    nav.goto(0, dist, 0.5, 1)
+    nav.goto(dist*-1, 0, 0.5, 1)
+    nav.goto(0, dist*-1, 0.5, 1)
 
 
 
 def initilize():
+    global vehicle
+    global nav
+
     try:
         vehicle = connect('127.0.0.1:14550', wait_ready=True)  #Connect to vehicle and initialize home location
         vehicle.home_location = vehicle.location.global_frame
+
+        nav = NavClass(vehicle)
+        print "Navigation initilized"
+
+        main()
     # Bad TCP connection
     except socket.error:
         print 'No server exists!'
@@ -88,5 +101,3 @@ def initilize():
     except Exception, e:
         print 'Unknown Error Has Occurred During Connection'
         print e.message
-    main(vehicle)
-
