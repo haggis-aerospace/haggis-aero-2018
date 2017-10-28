2205d99e0ec10eccee2e0ca8fcf2ca95834e8d13
DavidH <HeyItsDavid@users.noreply.github.com>
2017-10-28 14:19:10 +0100

    some skeleton files

--- cvlib/lib/library.cpp
+++ cvlib/lib/library.cpp
@@ -3,12 +3,9 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
 
-extern "C"
-{
     void hello() {
         std::cout << "Hello, World!" << std::endl;
     }
     void libtest() {
          std::cout << cv::getBuildInformation();
     }
-}
---
+++ cvlib/lib/scene.h
@@ -0,0 +1,22 @@
+//
+// Created by David Helekal on 22/10/2017.
+//
+
+/**
+ * OK so the thing is you instantiate this thing via a library static method, you then add analyzer objects to it
+ * which track / detect features in the scene
+ * The actual improc is done via a method accepting a frame or a stream thereof
+ */
+
+#ifndef CVLIB_SCENEPROCESSOR_H
+#define CVLIB_SCENEPROCESSOR_H
+class scene_processor {
+
+};
+/**
+ * Describes an object located in the scene
+ */
+class scene_object {
+
+};
+#endif //CVLIB_SCENEPROCESSOR_H
