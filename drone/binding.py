8676119707c60d3fd7727229aeb930f467b16043
Unknown <mike546378@gmail.com>
2018-01-26 00:03:39 +0000

    Python bindings and associated functions

--- drone/binding.py
+++ drone/binding.py
@@ -1,16 +1,59 @@
 from ctypes import *
 import os
 
-class lib:
 
-    libc = None
+libc = cdll.LoadLibrary("./libcvlib.so")
 
-    def __init__(self):
-        global libc
-        libc = cdll.LoadLibrary("../cvlib/lib/libcvlib.so")
+class Letter(Structure):
+    _fields_ = [
+        ('letter', c_char),
+        ('width', c_int),
+        ('height', c_int),
+        ('x', c_int),
+        ('y', c_int),
+        ('pos', c_int),
+        ('avSize',c_int)]
+
+
+class lib(object):
+
+    global lastLetter
+
+    def __init__(self, display=False):
+        global lastLetter
+        libc.py_camLib.argtypes = [c_bool]
+        libc.py_camLib.restype = c_void_p
+
+        libc.libtest.argtypes = []
+        libc.libtest.restype = c_void_p
+
+        libc.py_getImg.argtypes = [c_void_p]
+        libc.py_getImg.restype = c_void_p
+
+        libc.py_findLetter.argtypes = [c_void_p, c_int]
+        libc.py_findLetter.restype = c_void_p
+
+        libc.py_mostOccouring.argtypes = [c_void_p]
+        libc.py_mostOccouring.restype = Letter
+
+        lastLetter = Letter
+        self.obj = libc.py_camLib(display)
 
     def libtest(self):
-        global libc
-        call = libc.libtest
-        call.restype = c_void_p
-        print call()
+        print libc.libtest(self.obj)
+
+    def getImg(self):
+        return libc.py_getImg(self.obj)
+
+    def findLetters(self):
+        return libc.py_findLetter(self.obj, 75)
+
+    def mostOccouring(self):
+        global lastLetter
+        lastLetter = libc.py_mostOccouring(self.obj)
+        return lastLetter
+
+    def getLastLetter(self):
+        global lastLetter
+        return lastLetter
+
