a9b5f1c8e79a30a425f4f8f592214a9522a66504
Unknown <mike546378@gmail.com>
2018-02-06 18:29:13 +0000

    Tester program reads downStream.avi
    test params on saved video file

--- cvlib/test/demo.cpp
+++ cvlib/test/demo.cpp
@@ -1,43 +1,33 @@
-#include <iostream>
-#include "library.h"
-#include <stddef.h>
-#include <vector>
-#include <thread>
 #include <unistd.h>
+#include <sstream>
+#include <iostream>
+#include <cstdlib>
+#include "opencv2/opencv.hpp"
+#include <thread>
+#include <stdio.h>
+#include <string.h>
+#include "library.h"
+#include <X11/Xlib.h>
+
+using namespace std;
+using namespace cv;
+
 using namespace std;
 
 
 int main(int argc, char** argv ) {
-    /*
-    std::deque<double> frames;
-    camLib* camAPI = new camLib(true);
-    std::clock_t timer = std::clock();   //For calculating FPS
+    camLib library;
     
-    std::vector<Letter> lettrs;
-    Letter ltr;
-    while(true)
-    {
-        camAPI->findLetter(camAPI->getImg());
-        ltr = camAPI->mostOccouring();
-       
-        double fps = ( std::clock() - timer ) / (double) CLOCKS_PER_SEC * 100;
-        
-        printf("Found: %c at %i%% to the left. Size: %i,%i  Angle Ratio: %.2f\n", ltr.letter, ltr.pos, ltr.width, ltr.height, ((float)ltr.width/(float)ltr.height));
-        
-        //Calculate FPs
-        frames.push_front(60.0/fps);
-        if(frames.size() > 4)
-            frames.pop_back();
-        double totalfps = 0;
-        for(int i=0; i<frames.size(); i++)
-            totalfps = totalfps + frames[i];
-        fps = totalfps/frames.size();
-        printf("FPS: %.3f\n", fps);
-        
-        std::cout << "---" << std::endl;
-        
-        timer = std::clock();
+    while(true){
+        VideoCapture cap("downStream.avi");
+        Mat frame;
+        for(int i=0; i<1000; i++)
+            cap.grab();
+        cap >> frame;
+        while(frame.data)
+        {
+            cap >> frame;
+            library.findLetter(frame);
+        }
     }
-    */
-    cout << "No current demo avaliable" << endl;
 }
