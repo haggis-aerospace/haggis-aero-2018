39052e4ab69b19b72e3ba3bf246803c97e560e33
Unknown <mike546378@gmail.com>
2018-02-25 03:22:19 +0000

    Fixed demo playback too fast

--- cvlib/test/demo.cpp
+++ cvlib/test/demo.cpp
@@ -8,6 +8,7 @@
 #include <string.h>
 #include "library.h"
 #include <X11/Xlib.h>
+#include <sys/time.h>
 
 using namespace std;
 using namespace cv;
@@ -24,10 +25,18 @@ int main(int argc, char** argv ) {
         for(int i=0; i<1000; i++)
             cap.grab();
         cap >> frame;
+        clock_t last_cycle = clock();
+        unsigned int counter = 0;
         while(frame.data)
         {
             cap >> frame;
+            counter++;
+            cout << "Frame: " << counter << endl;
             library.findLetter(frame);
+            
+            clock_t next_cycle = clock();
+            double duration = (next_cycle - last_cycle) / CLOCKS_PER_SEC;
+            usleep((70000 - duration*1000));
         }
     }
 }
