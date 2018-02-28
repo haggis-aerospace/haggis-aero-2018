518e9f4546524fa9014a23a140ebe843f7779cce
Unknown <mike546378@gmail.com>
2018-02-28 03:34:22 +0000

    Lowered frame rate to save bandwidth

--- cvlib/network/config.h
+++ cvlib/network/config.h
@@ -1,6 +1,6 @@
 #define FRAME_HEIGHT 240
 #define FRAME_WIDTH 320
 #define FRAME_INTERVAL (1000/30)
-#define FRAME_RATE 60
+#define FRAME_RATE 20
 #define PACK_SIZE 6000 //udp pack size; note that OSX limits < 8100 bytes
 #define ENCODE_QUALITY 80
